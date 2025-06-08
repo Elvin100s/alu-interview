1a99ebdee302c77f8b8d8fee87ea815122dfa7c8
Elvin <e.cyubahiro@alustudent.com>
2025-06-08 22:47:17 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -2,31 +2,32 @@
 """
 Pascal's Triangle Module
 
-This module contains a function to generate Pascal's Triangle up to a given number of rows.
+Contains a function to generate Pascal's Triangle up to a given number of rows.
 """
+
 
 def pascal_triangle(n):
     """Generates Pascal's Triangle up to n rows
-    
+
     Args:
         n (int): Number of rows to generate
-        
+
     Returns:
         list: List of lists representing Pascal's Triangle
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         prev_row = triangle[-1]
         new_row = [1]
-        
+
         for j in range(1, i):
             new_row.append(prev_row[j-1] + prev_row[j])
-        
+
         new_row.append(1)
         triangle.append(new_row)
-    
+
     return triangle
